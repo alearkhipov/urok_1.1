01e4b7a870ab84a5e2b51bf2b06c65541b11dd51
alearkhipov <150228768+alearkhipov@users.noreply.github.com>
2024-10-14 17:10:28 +0300

    Update Main.java

--- src/main/java/ru/aleksey/Main.java
+++ src/main/java/ru/aleksey/Main.java
@@ -1,7 +1,10 @@
 package ru.aleksey;
 
 public class Main {
+
+    public static String msg ""Hello QA!!!!!!!!!"";
+    
     public static void main(String[] args) {
-        System.out.println("Hello QA!!!!!!!!!");
+        System.out.println(msg);
     }
 }
